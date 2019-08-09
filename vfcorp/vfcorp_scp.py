f1f3c644bf47430fb4a4d9e076bc37ae4eb04021
Raghavendran R <41452724+rravicha@users.noreply.github.com>
2019-08-09 14:32:30 +0530

    Add files via upload

--- vfcorp/vfcorp_scp.py
+++ vfcorp/vfcorp_scp.py
@@ -1,3 +1,35 @@
-import pandas as pd
-df=pd.read_excel("vfcorp_scp.xlsx",
+''''
+Build Type : Prototype
+Project Name : Supply Chain Planning
+'''
+#Needed imports
+try:
+    import pandas as pd
+except ImportError as e:
+    raise ImportError("package missing pandas")
+else:
+    print("successful import of pandas")
+finally:
+    print("End of pandas import")
 
+import sys
+#global variables
+
+#Class Definition
+class scp:
+    def __init__(self,filename,sheet):
+        self.filename=filename
+        self.sheet=sheet
+        
+    def read_sheet(self):
+        self.dmd=pd.read_excel(self.filename,sheet_name=self.sheet)
+        
+    def print_demand(self):
+        print(self.dmd)
+     
+##MAIN##
+if __name__ == "__main__":
+    scpobj = scp(sys.argv[0],sys.argv[0])
+    print("opening filename {} - sheetname {}".format(sys.argv[0],sys.argv[0]))
+    scpobj.read_sheet()
+    scpobj.print_demand()
