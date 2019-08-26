39be8127ae277ca30da634441768eb718b9db6e6
Raghavendran R <41452724+rravicha@users.noreply.github.com>
2019-08-26 10:40:39 +0530

    Add files via upload

--- vfcorp/vfcorp_scp.py
+++ vfcorp/vfcorp_scp.py
@@ -26,17 +26,33 @@ def line():
        print(l)
 #READ DEMAND FILE
 dmd=pd.read_excel(xlsx,dmdSheet)
-print(dmd)
+#print(dmd)
 #READ ONHAND FILE
 ohd=pd.read_excel(xlsx,onhandSheet)
-print(ohd)
+#print(ohd)
 #READ WIP FILE
 wip=pd.read_excel(xlsx,wipSheet)
-print(wip)
+#print(wip)
 line()
 #build HASHES
 dmd_d={}
 for i in range(len(dmd)):
-    dmd_k=str(dmd.values[i][0])+'~'+str(dmd.values[i][1])+'~'+str(dmd.values[i][2])+'~'+str(dmd.values[i][3])+'~'+str(dmd.values[i][4])
+    dmd_k=str(dmd.values[i][0])+'~'+str(dmd.values[i][1])+'~'+str(dmd.values[i][2])\
+                                                    +'~'+str(dmd.values[i][3])+'~'+str(dmd.values[i][4])
     dmd_v=dmd.values[i][-1]
     dmd_d[dmd_k]=dmd_v
+
+ohd_d={}
+for i in range(len(ohd)):
+    ohd_k=str(ohd.values[i][0])+'~'+str(ohd.values[i][1])+'~'+str(ohd.values[i][2])\
+                                                 +'~'+str(ohd.values[i][3])+'~'+str(ohd.values[i][4])
+    ohd_v=ohd.values[i][-1]
+    ohd_d[ohd_k]=ohd_v
+
+
+wip_d={}
+for i in range(len(wip)):
+    wip_k=str(wip.values[i][0])+'~'+str(wip.values[i][1])+'~'+str(wip.values[i][2])\
+                                                 +'~'+str(wip.values[i][3])+'~'+str(wip.values[i][4])
+    wip_v=wip.values[i][-1]
+    wip_d[wip_k]=wip_v
