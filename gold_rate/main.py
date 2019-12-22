71d5af5147fd1e0f01951306c8474c04b7178636
Raghavendran R <41452724+rravicha@users.noreply.github.com>
2019-12-23 00:42:16 +0530

    Add files via upload

---
+++ gold_rate/main.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+content1=[];content2=[];content3=[];content4=[];content5=[]
+url = "https://www.livechennai.com/gold_silverrate.asp"
+page=requests.get(url)
+data=bs(page.text,'html.parser')
+fnt=data.find_all('font')
+fnt=iter(fnt)
+
+next(fnt)
+next(fnt)
+
+try:
+    while(True):
+        # print(next(fnt).text)
+        this=next(fnt)
+        content1.append(this.text)
+except StopIteration:
+    pass
+    # print(content)
+
+with open("content.txt",'w') as fh:
+    fh.writelines(content1)
+with open("content.txt") as fh:
+    content1=fh.readlines()
+
+for i in range(11):
+    if i==0 or i>11:
+        pass
+    else:
+        content2.append(content1[i])
+
+content3=[row.strip() for row in content2]
+content4.append(['Date','24K_1gm','24K_8gm','22K_1gm','22K_8gm'])
+content4=[list((row[:16],row[16:23],row[23:31],row[31:38],row[38:47])) for row in content3]
+headers=('Date','24K_1gm','24K_8gm','22K_1gm','22K_8gm')
+# print(content4)
+df=pd.DataFrame(content4,columns=headers)
+# df.to_csv("content.csv")
+
+
+
+
+
+
+
+
+
+
+print("EOP")
