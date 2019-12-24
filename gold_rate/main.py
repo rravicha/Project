9dccee3a09bc7d9999c47400cc9d9aaf653e39c4
Raghavendran R <41452724+rravicha@users.noreply.github.com>
2019-12-24 10:54:49 +0530

    Add files via upload

--- gold_rate/main.py
+++ gold_rate/main.py
@@ -1,6 +1,9 @@
+import os
+os.system('clear')
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+import sqlite3
 content1=[];content2=[];content3=[];content4=[];content5=[]
 url = "https://www.livechennai.com/gold_silverrate.asp"
 page=requests.get(url)
@@ -35,17 +38,27 @@ content3=[row.strip() for row in content2]
 content4.append(['Date','24K_1gm','24K_8gm','22K_1gm','22K_8gm'])
 content4=[list((row[:16],row[16:23],row[23:31],row[31:38],row[38:47])) for row in content3]
 headers=('Date','24K_1gm','24K_8gm','22K_1gm','22K_8gm')
-# print(content4)
+# print(headers[0])
 df=pd.DataFrame(content4,columns=headers)
+
 # df.to_csv("content.csv")
+print(df)
+print('writing to db')
+# data = <This is going to be your pandas dataframe>
+
+conn = sqlite3.connect('gold_db')
+cur = conn.cursor()
+# cur.execute('''DROP TABLE IF EXISTS SA''')
+df.to_sql('gold_tb', conn, if_exists='replace', index=False) # - writes the pd.df to SQLIte DB
+q=pd.read_sql('select * from gold_tb', conn)
+print(q)
+conn.commit()
 
 
 
 
 
-
-
-
-
-
+# conn=sqlite3.connect('gold.db')
+# cur=conn.cursor()
+# cur.execute(''' INSERT INTO gold_tb values ''')
 print("EOP")
