6612017a8b887db5c9f90b5c867c4988a9bf5b62
Raghu <rravicha.2010@gmail.com>
2020-04-25 20:46:31 +0530

    scrap

--- giraffee/TicketDesk/TicketDesk.Web.Client/Scripts/ticketdesk/locale_en-US.js
+++
@@ -1,8 +0,0 @@
-﻿(function (window) {
-    window.i18n = {
-        unwatch : 'Unwatch',
-        watch : 'Watch',
-        formatWatch: function (isSubscribed) { return isSubscribed ? 'Unwatch' : 'Watch'; },
-        formatWatchTitle: function (isSubscribed) { return (isSubscribed ? 'Unwatch' : 'Watch') + ' Ticket'; }
-    };
-})(window);
--- giraffee/TicketDesk/TicketDesk.Web.Client/Scripts/ticketdesk/locale_pt-BR.js
+++
@@ -1,8 +0,0 @@
-﻿(function (window) {
-    window.i18n = {
-        unwatch: 'Esquecer',
-        watch: 'Acompanhar Atualizações',
-        formatWatch: function (isSubscribed) { return isSubscribed ? 'Esquecer' : 'Acompanhar Atualizações'; },
-        formatWatchTitle: function (isSubscribed) { return (isSubscribed ? 'Esquecer' : 'Acompanhar Atualizações') + ' do Chamado'; }
-    };
-})(window);
