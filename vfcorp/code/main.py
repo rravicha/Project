713f6ba568b3053e44acca30caf4a5fce6dc0f5d
Raghu <rravicha.2010@gmail.com>
2020-04-25 21:52:44 +0530

    arrange

---
+++ vfcorp/code/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+PPMS			: 	224259_001
+Project Title	:	Automate Non Ocr PDF formatting
+Program Name 	:	pdfocr_glob.py
+Execute Method 	:	Shell/Crontab
+Usage			:	`python3 pdfocr_glob.py`
+Input			:	Automatically takes files present in container directory
+Description		:	The program is part of automation initative to process the file that falls in 
+					container directory. The input file should be a Non-Ocr pdf file and the output
+					will be in the form of pdf
+'''
