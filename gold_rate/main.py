0b69345866723f6c405ccd09fb8591bd8dd4f6a6
Raghu <rravicha.2010@gmail.com>
2020-04-25 21:26:56 +0530

    gold commit

--- gold_rate/main.py
+++ gold_rate/main.py
@@ -2,7 +2,8 @@
 version notes:
 12-22-2019 - Initial Link to Production
 04-18-2020 - Added Database configuration to load resulting dataframe to db.
-04-20-2020 - 
+04-20-2020 - Defect#1 gold_list4 list malfunction fixed.
+04-22-2020 - Added logic to extract silver and load to database
 '''
 import os
 os.system('cls')
@@ -12,13 +13,7 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 import sys
-try:
-    flag=sys.argv[1]
-except IndexError:
-    flag=0
-
-print(flag)
-content1=[];content2=[];content3=[];content4=[];content5=[]
+gold_list1=[];gold_list2=[];gold_list3=[];gold_list4=[];gold_list5=[]
 url = "https://www.livechennai.com/gold_silverrate.asp"
 page=requests.get(url)
 data=bs(page.text,'html.parser')
@@ -27,48 +22,53 @@ fnt=iter(fnt)
 
 next(fnt)
 next(fnt)
-
+idx=0
 try:
     while(True):
         # print(next(fnt).text)
         this=next(fnt)
-        content1.append(this.text)
+        print(f"index {idx}")
+        print(this.text)
+        gold_list1.append(this.text)
+        idx+=1
 except StopIteration:
     pass
-    # print(content)
+    print(gold_list1)
 
-with open("content.txt",'w') as fh:
-    fh.writelines(content1)
-with open("content.txt") as fh:
-    content1=fh.readlines()
-
+with open("gold_list.txt",'w') as fh:
+    fh.writelines(gold_list1)
+with open("gold_list.txt") as fh:
+    gold_list1=fh.readlines()
+print('gold list 1 after formatting')
+print(gold_list1)
+'''
 for i in range(11):
     if i==0 or i>11:
         pass
     else:
-        content2.append(content1[i])
+        gold_list2.append(gold_list1[i])
 
-content3=[row.strip() for row in content2]
-content4.append(['date','24K_1gm','24K_8gm','22K_1gm','22K_8gm'])
-# content4=[list((row[:16],row[16:23],row[23:31],row[31:38],row[38:47])) for row in content3]
-content4=[list((row[:13],row[13:20],row[20:28],row[28:35],row[35:])) for row in content3]
+gold_list3=[row.strip() for row in gold_list2]
+gold_list4.append(['date','24K_1gm','24K_8gm','22K_1gm','22K_8gm'])
+# gold_list4=[list((row[:16],row[16:23],row[23:31],row[31:38],row[38:47])) for row in gold_list3]
+gold_list4=[list((row[:13],row[13:20],row[20:28],row[28:35],row[35:])) for row in gold_list3]
 headers=('date','24K_1gm','24K_8gm','22K_1gm','22K_8gm')
 # print(headers[0])
-df=pd.DataFrame(content4,columns=headers)
+df=pd.DataFrame(gold_list4,columns=headers)
 
-# df.to_csv("content.csv")
+# df.to_csv("gold_list.csv")
 print('raw df')
 print(df)
 print('format and replace')
 df.replace('',np.NaN,inplace=True)
 df.dropna(inplace=True)
 print(df)
-print('writing to db')
+print('writing to db')           
 
-if flag==1:
-    print('conneting to db')
-    engine   = create_engine("postgres://scott:tiger@localhost/bigdata")
-    df.to_sql('gold',engine,if_exists = 'append')   # writes to postgres db
+
+print('conneting to db')
+engine   = create_engine("postgres://scott:tiger@localhost/bigdata")
+df.to_sql('gold',engine,if_exists = 'append')   # writes to postgres db
 
 print('pushed to db')
 # q=pd.read_sql('select * from gold_tb', conn)
@@ -77,5 +77,19 @@ print('pushed to db')
 # print('commited the changes')
 # conn=sqlite3.connect('gold.db')
 # cur=conn.cursor()
-# cur.execute(''' INSERT INTO gold_tb values ''')
-print("EOP")
+
+'''
+print("Gold Rates loaded\n")
+
+print('Extracting for silver\n')
+silver_str=gold_list1[-1]
+print(silver_str)
+silver_list=silver_str[22:318].split(' ')
+silver_dict={}
+for row in silver_list:
+    key=row[0:13];value=row[13:18]
+    silver_dict[key]=value
+
+print("final dict")
+print(silver_dict)
+silver_headers=('date','rate')
