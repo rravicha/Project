d114a012046042113ad7c600dd4ab8e670aff580
Raghavendran R <rravicha.2008@gmail.com>
2023-03-12 10:28:45 +0530

    user.py updated

--- mini/ets/engine/user.py
+++ mini/ets/engine/user.py
@@ -1,14 +1,26 @@
+import faker
 import psycopg2
-conn=psycopg2.connect(
-  host="localhost",
-    database="ets",
-    user="postgres",
-    password="susi")
-
-db=conn.cursor()
+import psycopg2.extras as pe
+conn=psycopg2.connect(host="localhost",port=5432, database="ets", user="postgres", password="susi")
 conn.autocommit = True
+cur=conn.cursor()
+f=faker.Faker()
 
-db.execute('''select * from user;''')
+def populate_user_table():
+    sql=f'''
+    INSERT INTO dev."user"(name, email, dob, address, country, state, city, account_id)
+    VALUES(%s, %s, %s, %s, %s, %s,%s, %s)
+    '''
+    Values=(f.name(), f.email(), str(f.date_of_birth()), f.address(), f.country(), f.state(), f.city(), f.random_number())
+    try:
+        cur.execute(sql,Values)
+    except Exception as err:
+        print(f"general exception:{err}")
+        
 
-for i in db.fetchall():
-    print(i)
+
+populate_user_table()
+
+
+
+conn.close
